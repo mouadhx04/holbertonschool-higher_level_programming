65da71b161089c1de9d192b80e81c9bfde9a0eae
mouadhx04 <mouadh1500@gmail.com>
2021-09-28 14:07:10 -0700

    Write a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""square class
+"""
+
+
+class Square:
+    """ Square class defined by geometric shap
+        Attributes:
+            size (int): Size of square
+    """
+
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        set square square area
+        Return:
+            the current square area (int)
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        getter of size
+        Return:
+            Size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter of size
+        Args:
+            size (int): size of a side of the square
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """
+        print a square from the size using ##
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(1, self.__size + 1):
+                for j in range(1, self.__size + 1):
+                    print("#", end="")
+                print()
