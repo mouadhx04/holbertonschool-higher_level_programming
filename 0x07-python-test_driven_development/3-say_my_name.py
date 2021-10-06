4f3ac7072a106092b424df7de68ada8bf0fc6f7e
mouadhx04 <mouadh1500@gmail.com>
2021-10-05 18:33:29 -0700

    prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""This module defines the function that prints My name
+"""
+module to print names
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """ Function that prints a name.
-    Args:
-        first_name (str): First name.
-        last_name (str): Last name.
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    """prints name with custom type error"""
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        print('My name is {:} {:}'.format(first_name, last_name))
+    else:
+        raise TypeError('{:} must be a string'
+                        .format('last_name' if isinstance(first_name, str)
+                                else 'first_name'))
