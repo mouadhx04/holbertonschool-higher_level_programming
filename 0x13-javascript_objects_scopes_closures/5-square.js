30078bc0bef23c4b50d4c9ffc4d3d16b549b1b6e
mouadhx04 <mouadh1500@gmail.com>
2021-12-14 14:43:21 -0800

    Square + extends() + super()

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-}
-module.exports = Square;
+};
