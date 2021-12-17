e356b84db746c0595bf08c7fb708eef31db4ed8b
mouadhx04 <mouadh1500@gmail.com>
2021-12-16 16:01:01 -0800

    Inheritance using super()

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
+	  this.height;
+	  this.width;
+	  super(size, size);
   }
 };
