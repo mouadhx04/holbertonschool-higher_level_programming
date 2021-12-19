a452ead0b9ab8d8757dbcb3308a8ea974eff5ce9
mouadhx04 <mouadh1500@gmail.com>
2021-12-19 13:46:27 -0800

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" list all State objects from the database hbtn_0e_6_usa"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+"""
+list all State objects from the
+database hbtn_0e_6_usa
+"""
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import model_state
+from model_state import Base
+import sys
 
 if __name__ == '__main__':
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    mySession = Session()
+    for st_obj in mySession.query(model_state.State).\
+            order_by(model_state.State.id).all():
+        print("{}: {}".format(st_obj.id, st_obj.name))
 
-    session = Session()
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    mySession.close()
