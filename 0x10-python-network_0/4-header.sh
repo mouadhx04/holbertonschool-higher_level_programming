2d5334fbc6ae1850a16bd772b31db4b8a31eecae
root <root@Mouadh.localdomain>
2022-01-09 19:48:09 +0100

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H 'X-School-User-Id: 98'
+curl -sH "X-HolbertonSchool-User-Id:98" "$1"
