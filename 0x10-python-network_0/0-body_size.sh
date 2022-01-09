1969e7c711bc0a21f135fa04113159a9dfb079bb
root <root@Mouadh.localdomain>
2022-01-09 17:52:23 +0100

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take in a URL sends a request to it, and displays the size of the body of the response
-curl -so /dev/null -w '%{size_download}\n' "$1"
+curl -sI "$1" | grep 'Content-Length:' | cut -c 17-
