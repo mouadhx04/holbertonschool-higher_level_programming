0dfb310b0a96cb012152877e28aec7aeede9835d
root <root@Mouadh.localdomain>
2022-01-09 19:34:04 +0100

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# take in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep "Allow" | cut -d' ' -f2-
+# take in a URL and displays all HTTP methods the server will accept 
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
